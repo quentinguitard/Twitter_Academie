d1ceb6997f409406e3400b8299e80b201e65674d
Laetitia Pham Van Chien <laetitia.pham-van-chien@epitech.eu>
2018-01-10 14:04:29 +0100

    navbar

--- views/home.php
+++ views/home.php
@@ -25,49 +25,9 @@ $countRow = count($row);
 	<title>Tweet Academy</title>
 </head>
 <body>
-
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-
-			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-
-				<ul class="nav navbar-nav">
-					<li><a href="index.php?controller=home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Acceuil<span class="sr-only"></span></a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> Notifications</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Messages</a></li>
-					<li><img src="image/logo.png" /><a class="navbar-brand" href="#">Tweet Academy</a></li>
-				</ul>
-
-				<form class="navbar-form navbar-left">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Search">
-					</div>
-					<button type="submit" class="glyphicon glyphicon-search"></button>
-				</form>
-
-				<div class="btn-group">
-					<button type="button" class="btn btn-info">Tweeter</button>
-
-				</div>
-
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
-
-
+<?php
+include "nav-bar.php";
+?>
 
 	<div class="container text-center">    
 		<div class="row">
